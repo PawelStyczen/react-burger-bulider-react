f7d098aae4f53216d1dc9dbdf513e08c77d87383
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2020-09-11 22:10:17 +0200

    ingredient adding and removing with validation

--- src/components/Burger/BulidControls/BulidControls.js
+++ src/components/Burger/BulidControls/BulidControls.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import classes from './BulidControls.module.css'
-import BulidControl from './BulidControl/BulidControl'
+import React from "react";
+import classes from "./BulidControls.module.css";
+import BulidControl from "./BulidControl/BulidControl";
 
 const controls = [
-{label: 'Salad', type: 'Salad'},
-{label: 'Bacon', type: 'Bacon'},
-{label: 'Cheese', type: 'Cheese'},
-{label: 'Meat', type: 'Meat'},
+  { label: "Salad", type: "Salad" },
+  { label: "Bacon", type: "Bacon" },
+  { label: "Cheese", type: "Cheese" },
+  { label: "Meat", type: "Meat" },
 ];
 
 const bulidControls = (props) => (
-    <div className={classes.BulidControls}>
-        {controls.map(ctrl => (
-            <BulidControl key={ctrl.label} label={ctrl.label} />
-        ))}
-    </div>
+  <div className={classes.BulidControls}>
+    {controls.map((ctrl) => (
+      <BulidControl
+        key={ctrl.label}
+        label={ctrl.label}
+       
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
+    ))}
+  </div>
 );
 
 export default bulidControls;
--- src/containers/BurgerBulider/BurgerBulider.js
+++ src/containers/BurgerBulider/BurgerBulider.js
@@ -1,27 +1,73 @@
-import React, {Component} from 'react';
-import Burger from '../../components/Burger/Burger';
-import BulidControls from '../../components/Burger/BulidControls/BulidControls';
+import React, { Component } from "react";
+import Burger from "../../components/Burger/Burger";
+import BulidControls from "../../components/Burger/BulidControls/BulidControls";
+
+const INGREDIENT_PRICES = {
+  Salad: 0.5,
+  Cheese: 0.4,
+  Ceat: 1.7,
+  Bacon: 2,
+};
 
 class BurgerBulider extends Component {
-    
-    
-    state = {
-        ingredients: {
-            Salad:0,
-            Bacon: 0,
-            Cheese: 0,
-            Meat: 0
-        }
+  state = {
+    ingredients: {
+      Salad: 0,
+      Bacon: 0,
+      Cheese: 0,
+      Meat: 0,
+    },
+    totalPrice: 4,
+  };
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
     }
-    
-    render() {
-        return(
-            <React.Fragment>
-                <Burger ingredients={this.state.ingredients}/>
-                <BulidControls />
-            </React.Fragment>
-        );
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  render() {
+    const disabledInfo = {
+      ...this.state.ingredients,
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
     }
+    return (
+      <React.Fragment>
+        <Burger ingredients={this.state.ingredients} />
+        <BulidControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default BurgerBulider;
