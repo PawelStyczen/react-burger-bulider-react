1d6feced831a40485f01277712bf5a3ced700b96
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2020-09-13 19:23:03 +0200

    mobile navigation

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import classes from './Toolbar.module.css';
-import Logo from '../../Logo/Logo';
+import React from "react";
+import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
-           ...
-        </nav>
-    </header>
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+
+    <nav>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 export default toolbar;
