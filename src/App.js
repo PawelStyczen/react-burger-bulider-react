2e5e7dc7a26967bab81e324a08119733d88cb3f2
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2020-09-20 22:26:51 +0200

    order summary performance up, folder structure change, FINAL BULID 1.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Layout from './components/Layout/Layout'
+import Layout from './containers/Layout/Layout'
 import React, { Component } from "react";
 import BurgerBulider from './containers/BurgerBulider/BurgerBulider'
 
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,38 @@
-import React from "react";
-import Button from '../../UI/Button/Button'
+import React, { Component } from "react";
+import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+class OrderSummary extends Component {
+
+  // componentWillUpdate() {
+  //   console.log('[OrderSummary] will update')
+  // }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
+
     return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-        {props.ingredients[igKey]}
-      </li>
+      <React.Fragment>
+        <h3>Your Order</h3>
+        <p>A delicio</p>
+        <ul>{ingredientSummary}</ul>
+        <p>Total Price: {this.props.price.toFixed(2)} </p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </React.Fragment>
     );
-  });
-  return (
-    <React.Fragment>
-      <h3>Your Order</h3>
-      <p>A delicio</p>
-      <ul>
-          {ingredientSummary}
-      </ul>
-      <p>Total Price: {props.price.toFixed(2)} </p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </React.Fragment>
-  );
-};
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
