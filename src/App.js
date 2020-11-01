15407cde0015df5ad598ecb7a7e5252fc5292a7a
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2020-11-01 18:13:41 +0100

    created contactdata

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Layout from "./containers/Layout/Layout";
 import React, { Component } from "react";
+import {Route} from 'react-router-dom';
 import BurgerBulider from "./containers/BurgerBulider/BurgerBulider";
 import Checkout from "./containers/Checkout/Checkout";
 
@@ -8,8 +9,8 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBulider />
-          <Checkout />
+          <Route path="/" exact component={BurgerBulider} />
+          <Route path="/checkout" component={Checkout} />
         </Layout>
       </div>
     );
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,25 +1,49 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from "../../containers/Checkout/ContactData/ContactData";
 
 class Checkout extends Component {
-state = {
+  state = {
     ingredients: {
-        Salad: 1,
-        Meat: 1,
-        Cheese: 1,
-        Bacon: 1
-    }
-}
-    render() {
-        return (
-            <div>
-                <CheckoutSummary ingredients={this.state.ingredients}/>
-            </div>
-        )
+      Salad: 1,
+      Meat: 1,
+      Cheese: 1,
+      Bacon: 1,
+    },
+  };
 
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      //['salad', '1']
+      ingredients[param[0]] = +param[1];
     }
-    
+    this.setState({ ingredients: ingredients });
+  }
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
+
+  render() {
+    return (
+      <div>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route path={this.props.match.path + "/contact-data"} component={ContactData}/>
+      </div>
+    );
+  }
 }
 
 export default Checkout;
