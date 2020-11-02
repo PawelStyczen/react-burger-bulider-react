c453e9ed5dc4d0feac72980aabfd4f52e18bc888
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2020-11-02 20:30:29 +0100

    added and implemented Input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Spinner from "../../../components/UI/Spinner/Spinner";
 import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.module.css";
 import axios from "../../../axios-orders";
+import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends Component {
   state = {
@@ -37,7 +38,7 @@ class ContactData extends Component {
       .post("/orders.json", order)
       .then((response) => {
         this.setState({ loading: false });
-        this.props.history.push('/');
+        this.props.history.push("/");
       })
       .catch((error) => {
         this.setState({ loading: false });
@@ -47,26 +48,26 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input
-          className={classes.Input}
+        <Input
+          inputType="input"
           type="text"
           name="name"
           placeholder="Your name"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputType="input"
           type="email"
           name="email"
           placeholder="Your email"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputType="input"
           type="text"
           name="street"
           placeholder="street"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputType="input"
           type="text"
           name="postal"
           placeholder="Postal Code"
