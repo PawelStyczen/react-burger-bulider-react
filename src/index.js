22ef1c254445c0e2f193bdf9c8856567bf73a9a2
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2020-11-12 20:20:23 +0100

    installed middleware redux thunk

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 
 import burgerBuliderReducer from "./store/reducers/burgerBulider";
 
-const store = createStore(burgerBuliderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(burgerBuliderReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 const app = (
   <Provider store={store}>
