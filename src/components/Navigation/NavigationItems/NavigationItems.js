191e4316ecd6050607dc10170fbf6ef2600c72c0
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2020-11-20 20:03:22 +0100

    added logout function

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,11 +2,13 @@ import React from "react";
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 
-const navigationItems = () => (
+const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" exact> Burger Bulider</NavigationItem>
     <NavigationItem link="/orders"> Orders</NavigationItem>
-    <NavigationItem link="/auth" exact>Authentication</NavigationItem>
+{!props.isAuthenticated 
+? <NavigationItem link="/auth" exact>Authentication</NavigationItem> 
+: <NavigationItem link="/logout" exact>Logout</NavigationItem> }
   </ul>
 );
 
