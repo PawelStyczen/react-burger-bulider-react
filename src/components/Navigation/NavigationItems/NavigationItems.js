a008e9ae7be5d5c7843ba8ae50e8271a03a94c7f
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2020-11-22 18:48:21 +0100

    adding routing guards and persistent local token

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,26 +1,16 @@
-import React from "react";
-import classes from "./NavigationItems.module.css";
-import NavigationItem from "./NavigationItem/NavigationItem";
+import React from 'react';
 
-const navigationItems = (props) => (
-  <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" exact>
-      {" "}
-      Burger Bulider
-    </NavigationItem>
-    {props.isAuthenticated ? (
-      <NavigationItem link="/orders"> Orders</NavigationItem>
-    ) : null}
-    {!props.isAuthenticated ? (
-      <NavigationItem link="/auth" exact>
-        Authentication
-      </NavigationItem>
-    ) : (
-      <NavigationItem link="/logout" exact>
-        Logout
-      </NavigationItem>
-    )}
-  </ul>
+import classes from './NavigationItems.module.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = ( props ) => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+        {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
+        {!props.isAuthenticated
+            ? <NavigationItem link="/auth">Authenticate</NavigationItem>
+            : <NavigationItem link="/logout">Logout</NavigationItem>}
+    </ul>
 );
 
 export default navigationItems;
